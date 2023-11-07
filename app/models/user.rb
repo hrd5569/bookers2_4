b3b6f395257c682fe946e48cf8251f672b113ea7
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2023-11-07 03:59:32 +0000

    [Finish]

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+has_many :relationships, foreign_key: :follower_id
+has_many :followings, through: :relationships, source: :following
+
+has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :following_id
+has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  has_one_attached :profile_image
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+  
+  
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+end
